4d29644205557aec2381869912ce33678b4f4128
jubinjacob03 <jr.jubin03@gmail.com>
2024-06-14 02:46:39 +0530

    added new works

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -6,6 +6,10 @@ import Work5 from "../../assets/work-5.png";
 import Work6 from "../../assets/work-6.png";
 import Work7 from "../../assets/work-7.png";
 import Work8 from "../../assets/work-8.png";
+import Work9 from "../../assets/work-9.png";
+import Work10 from "../../assets/work-10.png";
+import Work11 from "../../assets/work-11.png";
+import Work12 from "../../assets/work-12.png";
 
 const Menu = [
   {
@@ -14,6 +18,7 @@ const Menu = [
     title: "Language Translator App",
     category: "Mobile App Development",
     link: "https://github.com/jubinjacob03/LangSHELF-Android_Studio-Kt_Google-API",
+    preview:""
   },
   {
     id: 2,
@@ -21,6 +26,7 @@ const Menu = [
     title: "TODO App Flask",
     category: "Full Stack",
     link: "https://github.com/jubinjacob03/TODO_Flask-Graphql_JWT_Stripe",
+    preview:""
   },
   {
     id: 3,
@@ -28,13 +34,15 @@ const Menu = [
     title: "React Dashboard",
     category: "Front-End",
     link: "https://github.com/jubinjacob03/Pisense-Frontend_ReactJS_ApexCharts",
+    preview:""
   },
   {
     id: 4,
     image: Work4,
     title: "Cardiac Disease Prediction",
-    category: "CYB / ML / DL/ AI / NLP / IOT",
+    category: "CYB / ML / DL / AI / IOT / Others",
     link: "https://github.com/jubinjacob03/HeartDiseaseClassify-ML/tree/heartD-1",
+    preview:""
   },
   {
     id: 5,
@@ -42,27 +50,63 @@ const Menu = [
     title: "Chat Application",
     category: "Mobile App Development",
     link: "https://github.com/jubinjacob03/ChatWIZ-Android_Studio-Kt_FireBase",
+    preview:""
   },
   {
     id: 6,
     image: Work6,
     title: "Face Recognition RaspBerryPI",
-    category: "CYB / ML / DL/ AI / NLP / IOT",
+    category: "CYB / ML / DL / AI / IOT / Others",
     link: "https://github.com/jubinjacob03/facedetection-RaspberryPi-IOT",
+    preview:""
   },
   {
     id: 7,
     image: Work7,
-    title: "Anime Site",
-    category: "Front-End",
-    link: "https://github.com/jubinjacob03/Java-JS_webpage",
+    title: "URL Shortner - AWS Lambda",
+    category: "Full Stack",
+    link: "https://github.com/jubinjacob03/shortify_url-shortner-aws_lambda",
+    preview:"https://url-shortner-aws.netlify.app/"
   },
   {
     id: 8,
     image: Work8,
+    title: "Genre Classification - KNN",
+    category: "CYB / ML / DL / AI / IOT / Others",
+    link: "https://github.com/jubinjacob03/genre-classification-recommendation_Spotify",
+    preview:""
+  },
+  {
+    id: 9,
+    image: Work9,
+    title: "Anime Site",
+    category: "Front-End",
+    link: "https://github.com/jubinjacob03/Java-JS_webpage",
+    preview:""
+  },
+  {
+    id: 10,
+    image: Work10,
+    title: "Cleanup Utility - Batch Script",
+    category: "CYB / ML / DL / AI / IOT / Others",
+    link: "https://github.com/jubinjacob03/cleanup_utility.bat-Win",
+    preview:""
+  },
+  {
+    id: 11,
+    image: Work11,
     title: "Machine Learning - Heart Diseases",
-    category: "CYB / ML / DL/ AI / NLP / IOT",
+    category: "CYB / ML / DL / AI / IOT / Others",
     link: "https://github.com/jubinjacob03/HeartDiseaseClassify-ML/tree/heartD-2",
+    preview:""
+  },
+  {
+    id: 12,
+    image: Work12,
+    title: "E-Commerce Site - NextJS",
+    category: "Full Stack",
+    link: "https://github.com/jubinjacob03/zingari-ecomm_frontend",
+    preview:""
   },
 ];
 export default Menu;
