82dd1729e3f443919f064ff65b9398d8e6276b49
jubinjacob03 <jr.jubin03@gmail.com>
2024-06-14 02:46:07 +0530

    removed contacts and added particles

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Resume from "./components/resume/Resume";
 import Portfolio from "./components/portfolio/Portfolio";
-import Contact from "./components/contact/Contact";
+import ParticleComponent from "./components/portfolio/particles/Particles";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <main className="main">
+        <ParticleComponent/>
         <Home />
         <About />
         <Resume />
         <Portfolio />
-        <Contact />
       </main>
     </div>
   );
---
+++ src/components/portfolio/particles/Particles.js
@@ -0,0 +1,93 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useMemo, useState } from "react";
+import { loadSlim } from "@tsparticles/slim";
+
+const ParticleComponent = (props) => {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "hsl(258, 60%, 98%)",
+        },
+      },
+      fpsLimit: 240,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "repulse",
+          },
+          onHover: {
+            enable: true,
+            mode: "grab",
+          },
+        },
+        modes: {
+          push: {
+            distance: 200,
+            duration: 15,
+          },
+          grab: {
+            distance: 150,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "hsl(244, 24%, 26%)",
+        },
+        links: {
+          color: "hsl(244, 24%, 26%)",
+          distance: 180,
+          enable: true,
+          opacity: 0.3,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: true,
+          speed: 1.15,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 1.0,
+        },
+        shape: {
+          type: "star",
+        },
+        size: {
+          value: { min: 2, max: 3 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  return <Particles id={props.id} init={particlesLoaded} options={options} />;
+};
+
+export default ParticleComponent;
