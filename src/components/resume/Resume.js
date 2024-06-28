b2064b81989385e54c998c9d1a3be6b879d0caf2
jubinjacob03 <jr.jubin03@gmail.com>
2024-06-29 03:26:59 +0530

    int. cardWrapper for experience sect.

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -2,45 +2,51 @@ import React from "react";
 import "./resume.css";
 import Data from "./Data";
 import Card from "./Card";
+import { CardWrapper } from "./CardWrapper";
 
 const Resume = () => {
   return (
     <section className="resume container section" id="resume">
       <h2 className="section-title">Experience</h2>
-      <div className="resume-container grid">
-        <div className="timeline grid">
-          {Data.map((val, id) => {
-            if (val.category === "education") {
-              return (
-                <Card
-                  key={id}
-                  icon={val.icon}
-                  title={val.title}
-                  year={val.year}
-                  desc={val.desc}
-                />
-              );
-            }
-            return null; // Return a value for other cases
-          })}
-        </div>
 
-        <div className="timeline grid">
-          {Data.map((val, index) => {
-            if (val.category === "experience") {
-              return (
-                <Card
-                  key={index}
-                  icon={val.icon}
-                  title={val.title}
-                  year={val.year}
-                  desc={val.desc}
-                />
-              );
-            }
-            return null; // Return a value for other cases
-          })}
-        </div>
+      <div className="resume-container grid">
+        <CardWrapper>
+          <div className="timeline grid">
+            {Data.map((val, id) => {
+              if (val.category === "education") {
+                return (
+                  <Card
+                    key={id}
+                    icon={val.icon}
+                    title={val.title}
+                    year={val.year}
+                    desc={val.desc}
+                  />
+                );
+              }
+              return null; // Return a value for other cases
+            })}
+          </div>
+        </CardWrapper>
+
+        <CardWrapper>
+          <div className="timeline grid">
+            {Data.map((val, index) => {
+              if (val.category === "experience") {
+                return (
+                  <Card
+                    key={index}
+                    icon={val.icon}
+                    title={val.title}
+                    year={val.year}
+                    desc={val.desc}
+                  />
+                );
+              }
+              return null; // Return a value for other cases
+            })}
+          </div>
+        </CardWrapper>
       </div>
     </section>
   );
