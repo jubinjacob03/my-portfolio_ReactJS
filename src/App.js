dfe939584cffe34c453143c8a515ca9b713dc093
jubinjacob03 <jr.jubin03@gmail.com>
2024-07-13 03:18:51 +0530

    Added contact wrapper in App.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import About from "./components/about/About";
 import Resume from "./components/resume/Resume";
 import Portfolio from "./components/portfolio/Portfolio";
 import ParticleComponent from "./components/portfolio/particles/Particles";
+import GlowingDots from "./components/contact/GlowingDots";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <main className="main">
-        <ParticleComponent/>
+        <ParticleComponent />
         <Home />
         <About />
         <Resume />
         <Portfolio />
+        <GlowingDots />
       </main>
     </div>
   );
